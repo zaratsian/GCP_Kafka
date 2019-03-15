7cf7565cb4e7a01785b8784c2933d9967fbe9255
Dan Zaratsian <dtz001@gmail.com>
2019-03-15 12:00:54 -0400

    Update 03_firewall_kafka.sh

--- kafka_simulator_threaded/03_firewall_kafka.sh
+++ kafka_simulator_threaded/03_firewall_kafka.sh
@@ -4,10 +4,10 @@ project_id=$GOOGLE_CLOUD_PROJECT
 
 gcloud compute --project=$project_id firewall-rules \
         create kafka-firewall \
+        --description="Allows requests from GKE to be sent to a Kafka Bootstrap Server GCE Instance" \
         --direction=INGRESS \
         --priority=1000 \
         --network=default \
         --action=ALLOW \
         --rules=tcp:9092 \
-        --source-ranges=0.0.0.0/0 \
-        --target-tags=kafka
+        --source-ranges=0.0.0.0/0
